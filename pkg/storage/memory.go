63c63ed1fd7c009d4be85e0810a03528db37f36d
Josue Munezero <josuemunezero29@gmail.com>
2026-01-26 05:53:33 -0500

    Added Put, Delete, and Close functions, Used defensive copy in Put and Get

--- pkg/storage/memory.go
+++ pkg/storage/memory.go
@@ -6,13 +6,26 @@ import (
 	"strings"
 )
 
+// MemStore is an in-memory implementation of Store
 type MemStore struct {
-	kvstore map[string][]byte
+	data map[string][]byte
 	mut sync.RWMutex
 	closed bool
 }
 
-func  (mem MemStore) Get(ctx context.Context, key string) ([]byte, error) {
+// NewMemStore creates an new in-memory store
+func NewMemStore () *MemStore {
+	return &MemStore{
+		data: make(map[string][]byte),
+	}
+}
+
+// Get returns a value by key
+func  (mem *MemStore) Get (ctx context.Context, key string) ([]byte, error) {
+	if strings.TrimSpace(key) == "" {
+		return nil, ErrInvalidKey
+	}
+
 	mem.mut.Lock()
 	defer mem.mut.Unlock()
 
@@ -21,17 +34,59 @@ func  (mem MemStore) Get(ctx context.Context, key string) ([]byte, error) {
 		return nil, ErrStoreClosed 
 	}
 
-	if strings.TrimSpace(key) == "" {
-		return nil, ErrInvalidKey
-	} 
-
-	if val, ok := mem.kvstore[strings.TrimSpace(key)]; ok {
-		return val, nil
+	if val, ok := mem.data[strings.TrimSpace(key)]; ok {
+		// Defensive copy
+		snapshot := make([]byte, len(val))
+		copy(snapshot, val)
+		return snapshot, nil
 	} else {
 		return nil, ErrKeyNotFound
 	}
 }
 
-func (mem MemStore) Put(ctx context.Context, key string, value []byte) error {
+// Put stores a key-value pair
+func (mem *MemStore) Put (ctx context.Context, key string, value []byte) error {
+	if strings.TrimSpace(key) == "" {
+		return ErrInvalidKey
+	}
+
+	mem.mut.RLock()
+	defer mem.mut.RUnlock()
+	
+	if mem.closed {
+		return ErrStoreClosed
+	}
+	
+	// Defensive copy
+	snapshot := make([]byte, len(value))
+	copy(snapshot, value)
+	mem.data[key] = snapshot
+	return nil
+}
+
+// Delete removes a key
+func (mem *MemStore) Delete(ctx context.Context, key string) error {
+	if strings.TrimSpace(key) == "" {
+		return ErrInvalidKey
+	}
+
+	mem.mut.Lock()
+	defer mem.mut.Unlock()
+
+	delete(mem.data, key)
+	return nil
+}
+
+// Close closes the store
+func (mem *MemStore) Close () error {
+	mem.mut.Lock()
+	defer mem.mut.Unlock()
+
+	if mem.closed {
+		return ErrStoreClosed
+	}
+
+	mem.closed = true
+	mem.data = nil
 	return nil
 }
