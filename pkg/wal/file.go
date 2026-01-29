8165cbb96ef43e124c342b236e6de849ebe4f895
Josue Munezero <josuemunezero29@gmail.com>
2026-01-29 06:02:13 -0500

    renamed writer interface to WALWriter

--- pkg/wal/file.go
+++ pkg/wal/file.go
@@ -5,7 +5,7 @@ import (
 )
 
 // WAL File Writer interface
-type Writer interface {
+type WALWriter interface {
 	// Log entry is not flushed to the file immediately
 	// File is not always in sync
 	Write(ctx context.Context, entry *LogEntry) error
